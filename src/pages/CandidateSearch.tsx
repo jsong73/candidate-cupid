f28ccf60678378fee292db900db6959d6d5634b1
jsong73 <jessicajinsong@gmail.com>
2024-06-26 23:39:36 -0400

    got res back from search all users

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,30 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+  const [ results, setResults] = useState<Candidate[]>([])
+
+  
+  const searchAllUsers = async () => {
+    try{
+      const res: Candidate[]= await searchGithub()
+      console.log("res:", res)
+
+      setResults(res)
+
+    } catch(error){
+      console.log("error getting results")
+    }
+
+  }
+
+  useEffect (() => {
+    searchAllUsers();
+  }, [])
+
   return <h1>CandidateSearch</h1>;
+
 };
 
 export default CandidateSearch;
