a640c8d8f5967fbb3742b3f8a714729e8ca4e403
jsong73 <jessicajinsong@gmail.com>
2024-07-07 16:40:17 -0400

    navbar active

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+
   return (
-    <div>Nav</div>
+    <nav> 
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink 
+            to="/"
+            className="nav-link"> Home </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink 
+          to="/SavedCandidates"
+          className="nav-link"> Potential Candidates</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
